483cd716f490b48a7f4c17b91ce22a097388f1f6
Joel <joel.eu.lopes@gmail.com>
2021-06-16 21:27:03 +0100

    view

--- Spring/src/main/java/com/joelito/spring/Controller/AguaController.java
+++ Spring/src/main/java/com/joelito/spring/Controller/AguaController.java
@@ -4,13 +4,15 @@ package com.joelito.spring.Controller;
 import com.joelito.spring.DAL.AguaEntity;
 import com.joelito.spring.Service.AguaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(path = "/agua")
 public class AguaController {
     private final AguaService aguaService;
@@ -21,7 +23,14 @@ public class AguaController {
     }
 
     @GetMapping
-    public List<AguaEntity> getAguaEntity(){
-        return aguaService.getAguaEntity();
+    public String getAgua(Model model){
+        model.addAttribute("messages",aguaService.getAguaEntity());
+        model.addAttribute("idagua",aguaService.getAguaEntity().get(0).getIdagua());
+        return "AguaView";
     }
+
+
+    /*public List<AguaEntity> getAguaEntity(){
+        return aguaService.getAguaEntity();
+    }*/
 }
